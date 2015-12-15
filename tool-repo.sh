8644562dc1c10bede17f52ec979ce1b4d34e4108
tentpegbob <tentpegbob@users.noreply.github.com>
2015-12-14 21:11:08 -0500

    Update tool-repo.sh

--- tool-repo.sh
+++ tool-repo.sh
@@ -1,22 +1,7 @@
 #!/bin/bash
 
 # Install GDB-Peda
-echo '# Source all settings from the peda dir' >> ~/.gdbinit
-echo 'source ~/peda/peda.py' >> ~/.gdbinit
-echo '' >> ~/.gdbinit
-echo '# These are other settings I have found useful' >> ~/.gdbinit
-echo '' >> ~/.gdbinit
-echo '# Intel syntax is more readable' >> ~/.gdbinit
-echo 'set disassembly-flavor intel' >> ~/.gdbinit
-echo ''
-echo '# When inspecting large portions of code the scrollbar works better than \'less\''>> ~/.gdbinit
-echo 'set pagination off' >> ~/.gdbinit
-echo '' >> ~/.gdbinit
-echo '# Keep a history of all the commands typed. Search is possible using ctrl-r' >> ~/.gdbinit
-echo 'set history save on' >> ~/.gdbinit
-echo 'set history filename ~/.gdb_history' >> ~/.gdbinit
-echo 'set history size 32768' >> ~/.gdbinit
-echo 'set history expansion on' >> ~/.gdbinit
+perl -e 'print "# Source all settings from the peda dir\n" . "source ~/peda/peda.py\n\n" . "# These are other settings I have found useful\n\n" . "# Intel syntax is more readable\n" . "set disassembly-flavor intel\n\n" . "# When inspecting large portions of code the scrollbar works better than \'less\'\n" . "set pagination off\n\n" . "# Keep a history of all the commands typed. Search is possible using ctrl-r\n" . "set history save on\n" . "set history filename ~/.gdb_history\n" . "set history size 32768\n" . "set history expansion on" > ~/.gdbinit
 cd /opt/ && git clone https://github.com/longld/peda && sed -i 's/: (\"off\"/: (\"on\"/g' /opt/peda/lib/config.py
 
 # Install Network Miner
