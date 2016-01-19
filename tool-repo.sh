0ae0e8f62e778b8f4e34a6a0e8ab13801ad4132b
tentpegbob <tentpegbob@users.noreply.github.com>
2016-01-18 22:33:13 -0500

    Update tool-repo.sh

--- tool-repo.sh
+++ tool-repo.sh
@@ -2,7 +2,7 @@
 
 # Install GDB-Peda
 perl -e 'print "# Source all settings from the peda dir\n" . "source ~/peda/peda.py\n\n" . "# These are other settings I have found useful\n\n" . "# Intel syntax is more readable\n" . "set disassembly-flavor intel\n\n" . "# When inspecting large portions of code the scrollbar works better than \"less\"\n" . "set pagination off\n\n" . "# Keep a history of all the commands typed. Search is possible using ctrl-r\n" . "set history save on\n" . "set history filename ~/.gdb_history\n" . "set history size 32768\n" . "set history expansion on"' > ~/.gdbinit
-cd /opt/ && git clone https://github.com/longld/peda && sed -i 's/: (\"off\"/: (\"on\"/g' /opt/peda/lib/config.py
+cd /opt/ && git clone https://github.com/longld/peda && sed -i 's/: (\"off\"/: (\"on\"/g' /opt/peda/lib/config.py && sed -i 's/~\/peda\/peda.py/\/opt\/peda\/peda.py/g' ~/.gdbinit
 
 # Setup Custom VIM Config
 wget https://raw.githubusercontent.com/tentpegbob/bearded-cyril/master/default-vim-cfg -O ~/.vimrc
@@ -65,6 +65,10 @@ sudo apt-get install --fix-missing -y lib32z1 lib32z1-dev libc6 libc6-dbg libc6-
 ## Install ARM support
 # Not Tested Yet
 # sudo apt-get install --fix-missing -y libc6-armel-cross libc6-armel-armhf-cross libc6-arm64-cross
+## strace
+apt-get install -y --fix-missing strace
+## valgrind
+apt-get install -y --fix-missing valgrind
 
 # Update the VM
 sudo apt-get update -y; sudo apt-get dist-upgrade -y
